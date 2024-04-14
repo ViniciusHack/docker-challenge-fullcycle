a9628696408505ceae476cbd80cd950e258b4ab4
vinicius <viniciusvwh1441@gmail.com>
2024-04-14 10:01:45 -0300

    feat: mysql integration

--- node/app/src/app.js
+++ node/app/src/app.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express(); 
 
+// mysql db connection
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'mysql_db',
+  user: 'app',
+  password: 'app',
+  database: 'app',
+});
+
+const sql = `INSERT INTO people(name) VALUES('Vini')`;
+connection.query(sql);
+
 app.get('/', (req, res) => {
-  return res.send('Full Cycle Rocks!');
+  connection.query('SELECT * FROM people', (err, result) => {
+    if (err) throw err;
+
+    const template = `
+      <div>
+        <h1>Full Cycle Rocks!</h1>
+        <h2>Users:</h2>
+        <ul>
+          ${result.map(item => `<li>${item.name}</li>`).join('')}
+        </ul>
+      </div>
+    `
+
+    return res.send(template);
+  });
 })
 
 app.listen(3000, () => {
